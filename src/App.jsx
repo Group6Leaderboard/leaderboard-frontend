c02bc7af7a3fe4ba0e25a24586e48b91bf415c31
Ihsana <ihsanamk109@gmail.com>
2025-03-24 15:38:43 +0530

    dashb

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom"; // Import useLocation here
 import DashboardLayout from "./Layouts/Dashboard/DashboardLayout"; // Import DashboardLayout
 import AdminDashboard from "./Pages/AdminDashboard";
-// import MentorDashboard from "./Pages/MentorDashboard";
-// import StudentDashboard from "./Pages/StudentDashboard";
-// import CollegeDashboard from "./Pages/CollegeDashboard";
+import MentorDashboard from "./Pages/MentorDashboard";
+import StudentDashboard from "./Pages/StudentDashboard";
+import CollegeDashboard from "./Pages/CollegeDashboard";
 import AssignForm from "./Components/AssignForm/AssignForm";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import CollegeLeaderboard from "./pages/Leaderboard/CollegeLeaderboard";
+import ProjectLeaderboard from "./pages/Leaderboard/ProjectLeaderboard";
+import StudentLeaderboard from "./pages/Leaderboard/StudentLeaderboard";
 
 const App = () => {
-  const location = useLocation(); 
-  const hideSidebarRoutes = ["/login","/students","/colleges","/projects"]; 
-  
+  const location = useLocation(); // Get the current route
+
+  // Routes where the sidebar should not be visible
+  const hideSidebarRoutes = ["/login", "/students", "/colleges", "/projects"];
 
   return (
-    <DashboardLayout>
-    <Routes>
-    <Route path="/admin" element={<AdminDashboard />} />
-    <Route path="/admin/assign-project" element={<AssignForm role="admin" />} />
-        {/* <Route path="/admin" element={<AdminDashboard />} />
+      <Routes>
+        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/assign-project" element={<AssignForm role="admin" />} />
-
         <Route path="/mentor" element={<MentorDashboard />} />
         <Route path="/mentor/assign-task" element={<AssignForm role="mentor" />} />
-
         <Route path="/student" element={<StudentDashboard />} />
-        <Route path="/college" element={<CollegeDashboard />} /> */}
- 
-    </Routes>
-    </DashboardLayout>
+        <Route path="/college" element={<CollegeDashboard />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/colleges" element={<CollegeLeaderboard />} />
+        <Route path="/projects" element={<ProjectLeaderboard />} />
+        <Route path="/students" element={<StudentLeaderboard />} />
+      </Routes>
   );
 };
 
